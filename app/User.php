19ff51eaabe353d324dc8fec99e4e5c0cc743367
Stephen <stephen.frx@gmail.com>
2015-12-25 13:48:41 +0800

    ignore user remember token

--- app/User.php
+++ app/User.php
@@ -4,23 +4,50 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+class User extends Authenticatable {
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'zh', 'mm',
+	];
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'mm',
+	];
+
+	protected $connection = 'pgsql';
+
+	protected $table = "xk_xsmm";
+
+	public function getRememberToken() {
+		return null; // not supported
+	}
+
+	public function setRememberToken($value) {
+		// not supported
+	}
+
+	public function getRememberTokenName() {
+		return null; // not supported
+	}
+
+	/**
+	 * Override the method to ignore the remember token
+	 */
+	public function setAttribute($key, $value) {
+		$isRememberTokenAttribute = $key == $this->getRememberTokenName();
+
+		if (!$isRememberTokenAttribute) {
+			parent::setAttribute($key, $value);
+		}
+	}
 }
